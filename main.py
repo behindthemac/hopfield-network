e1bc8f82e8947f943cf1d7d48441b71491aa14b1
Keisuke Miura <miura@ginjo.t.u-tokyo.ac.jp>
2020-04-24 21:27:55 +0900

    implement a Hopfield network

---
+++ main.py
@@ -0,0 +1,21 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from hopfield_network import HopfieldNetwork
+
+
+def main():
+    n = 10
+
+    hopfield_network = HopfieldNetwork(n)
+
+    energy = [hopfield_network.energy]
+    while not hopfield_network.is_local_minimum():
+        hopfield_network.update()
+        energy.append(hopfield_network.energy)
+
+    plt.plot(energy)
+    plt.show()
+
+
+if __name__ == '__main__':
+    main()
